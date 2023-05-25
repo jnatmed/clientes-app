aba0647f9d049b8f8987dd7325888aaee7dc8959
jnatmed <juanmannat@gmail.com>
2023-05-24 23:57:48 -0300

    laburando con ajaxs ?
    lo quen trato de hacer es enviar el formulario pero como tengo que capturar el post sin usar un router, me las tengo que ingeniar de alguna forma. En este caso, usando alguna especie de Ajaxs, espero funcione

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,103 @@
+
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1>Hola mundo</h1>
+<div class="">
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium similique aliquid error quas, ipsa quos nihil non amet repudiandae est veritatis officiis quaerat recusandae animi temporibus dignissimos, incidunt ad quam?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nulla ad. Dolorem numquam quos magnam nam et nulla deserunt illum, ab maxime, facilis dicta recusandae sequi reiciendis! Ipsam, facilis debitis?
+Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, nulla hic ipsum adipisci animi distinctio et aut saepe laudantium nobis earum perspiciatis, nisi doloribus! Reprehenderit obcaecati laboriosam natus soluta consequuntur?
+</div>
 `;
 
 const app = document.querySelector<HTMLDivElement>('#app');
 const home = document.querySelector<HTMLAnchorElement>('#home');
-const contactanos = document.querySelector<HTMLAnchorElement>('#contactanos');
-const ayuda = document.querySelector<HTMLAnchorElement>('#ayuda');
+const clientes = document.querySelector<HTMLAnchorElement>('#clientes');
+const crearCliente = document.querySelector<HTMLAnchorElement>('#crearCliente');
+
 
 home?.addEventListener('click', () => {
-	app!.innerHTML = `<h1> Hola desde Home </h1>`;
+	app!.innerHTML = `
+	<div class="">
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium similique aliquid error quas, ipsa quos nihil non amet repudiandae est veritatis officiis quaerat recusandae animi temporibus dignissimos, incidunt ad quam?
+	Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nulla ad. Dolorem numquam quos magnam nam et nulla deserunt illum, ab maxime, facilis dicta recusandae sequi reiciendis! Ipsam, facilis debitis?
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, nulla hic ipsum adipisci animi distinctio et aut saepe laudantium nobis earum perspiciatis, nisi doloribus! Reprehenderit obcaecati laboriosam natus soluta consequuntur?
+  </div>
+	`;
 	app!.classList.add('animate__animated');
 	app!.classList.add('animate__bounce');
 	// TODO: optimizar
-	contactanos!.classList.remove('active');
-	ayuda!.classList.remove('active');
+	clientes!.classList.remove('active');
+	crearCliente!.classList.remove('active');
 	home!.classList.add('active');
 });
 
-contactanos?.addEventListener('click', () => {
-	app!.innerHTML = `<div class="input-group animate__fadeIn">
-  <span class="input-group-text">First and last name</span>
-  <input type="text" aria-label="First name" class="form-control">
-  <input type="text" aria-label="Last name" class="form-control">
-</div>`;
+clientes?.addEventListener('click', () => {
+	app!.innerHTML = `<h1> Aqui muestro la lista de clientes </h1>`;
 
 	app!.classList.add('animate__animated');
 	app!.classList.add('animate__bounce');
 	// TODO: optimizar
 	home!.classList.remove('active');
-	ayuda!.classList.remove('active');
-	contactanos!.classList.add('active');
+	crearCliente!.classList.remove('active');
+	clientes!.classList.add('active');
 });
 
-ayuda?.addEventListener('click', () => {
+crearCliente?.addEventListener('click', () => {
+
+	const form = document.createElement('form');
+	const divgroup = document.createElement('div');
+	divgroup.className = 'input-group mb-3';
+	const spanName = document.createElement('span');
+	spanName.className = 'input-group-text';
+	spanName.innerHTML = 'First name';
+	const inputName = document.createElement('input');
+	inputName.id = 'first_name';
+	const btnenviar = document.createElement('button');
+	btnenviar.innerHTML = 'Enviar Datos'
+	inputName.className = 'form-control';
+	divgroup.appendChild(spanName);
+	divgroup.appendChild(inputName);
+	form.appendChild(divgroup);
+	form.appendChild(btnenviar);
+
+	app!.innerHTML = '';
+	app!.appendChild(form);
+
+	btnenviar?.addEventListener('click', (e : Event) => {
+		console.log("ALGO..");
+		const first_name = document.querySelector<HTMLInputElement>('#first_name');
+		console.log(first_name?.value);
+		var data = new FormData();
+		data.append('first_name', `${first_name?.value}` );
+		// data.append('pwd', 'password');
+		// data.append('organization', 'place');
+		// data.append('requiredkey', 'key');
+		console.log(data);
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', 'http://127.0.0.1:5173/?#', true);
+		xhr.onload = function () {
+			// do something to response
+			console.log(this.responseText);
+		};
+		xhr.send(data);
+	});
+
 	app!.classList.add('animate__animated');
 	app!.classList.add('animate__bounce');
-	app!.innerHTML = `<h1> Hola desde Ayuda</h1>`;
 	// TODO: optimizar
 	home!.classList.remove('active');
-	contactanos!.classList.remove('active');
-	ayuda!.classList.add('active');
+	clientes!.classList.remove('active');
+	crearCliente!.classList.add('active');
 });
+
+
+/**
+ first_name
+ last_name
+ phone
+ empresa
+ cuil
+ pais
+ localidad
+ cp
+ direccion
+ */
+
