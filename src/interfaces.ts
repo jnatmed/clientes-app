c1f19898752a7c8d6fbe14a46db9699e8c4bc5ca
jnatmed <49349742+jnatmed@users.noreply.github.com>
2023-06-01 00:08:04 -0300

    get y post funcionando
    no logro darle estilos a la tabla

---
+++ src/interfaces.ts
@@ -0,0 +1,11 @@
+export interface formulario {
+    first_name : string;
+    last_name : string;
+    phone : number;
+    empresa : string;
+    cuil : string; 
+    pais :string;
+    localidad : string;
+    cp : string;
+    direccion :string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { formulario } from './interfaces';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div class="">
@@ -14,13 +16,9 @@ const crearCliente = document.querySelector<HTMLAnchorElement>('#crearCliente');
 
 
 home?.addEventListener('click', () => {
-	app!.innerHTML = `
-	<div class="">
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium similique aliquid error quas, ipsa quos nihil non amet repudiandae est veritatis officiis quaerat recusandae animi temporibus dignissimos, incidunt ad quam?
-	Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, nulla ad. Dolorem numquam quos magnam nam et nulla deserunt illum, ab maxime, facilis dicta recusandae sequi reiciendis! Ipsam, facilis debitis?
-	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae, nulla hic ipsum adipisci animi distinctio et aut saepe laudantium nobis earum perspiciatis, nisi doloribus! Reprehenderit obcaecati laboriosam natus soluta consequuntur?
-  </div>
-	`;
+
+	app!.innerHTML = '';
+	
 	app!.classList.add('animate__animated');
 	app!.classList.add('animate__bounce');
 	// TODO: optimizar
@@ -29,8 +27,54 @@ home?.addEventListener('click', () => {
 	home!.classList.add('active');
 });
 
-clientes?.addEventListener('click', () => {
-	app!.innerHTML = `<h1> Aqui muestro la lista de clientes </h1>`;
+clientes?.addEventListener('click', (e) => {
+	e.preventDefault();
+	
+	axios.get('https://api-user-production.up.railway.app/usuarios')
+	.then(function (response) {
+		// handle success
+		app!.innerHTML = '';
+
+		let tabla = document.createElement("table");
+		tabla.className = 'blueTable';
+		let tblBody = document.createElement("tbody");
+		let thead = document.createElement("thead");
+		thead.innerHTML = `
+			<tr>
+			<th>Nombre</th>
+			<th>Apellido</th>
+			<th>Numero de Telefono</th>
+			<th>Pais</th>
+			<th>Localidad</th>
+			<th>Codigo Postal</th>
+			<th>Direccion</th>
+			</tr>
+		`;
+		tabla.appendChild(thead);
+		response['data'].forEach((element: any) => {
+			let fila = document.createElement('tr');
+			fila.appendChild(crearNodo(element.nombre));
+			fila.appendChild(crearNodo(element.apellido));
+			fila.appendChild(crearNodo(element.numeroTelefono));			
+			fila.appendChild(crearNodo(element.pais));			
+			fila.appendChild(crearNodo(element.localidad));			
+			fila.appendChild(crearNodo(element.codPostal));
+			fila.appendChild(crearNodo(element.direccion));			
+			tblBody.appendChild(fila);
+		});
+		tabla.appendChild(tblBody);
+		tabla.setAttribute("border", "2");
+
+		app!.appendChild(tabla);
+	})
+	.catch(function (error) {
+		// handle error
+		console.log(error);
+	})
+	.finally(function () {
+		// always executed
+	});
+
 
 	app!.classList.add('animate__animated');
 	app!.classList.add('animate__bounce');
@@ -42,42 +86,75 @@ clientes?.addEventListener('click', () => {
 
 crearCliente?.addEventListener('click', () => {
 
-	const form = document.createElement('form');
-	const divgroup = document.createElement('div');
-	divgroup.className = 'input-group mb-3';
-	const spanName = document.createElement('span');
-	spanName.className = 'input-group-text';
-	spanName.innerHTML = 'First name';
-	const inputName = document.createElement('input');
-	inputName.id = 'first_name';
-	const btnenviar = document.createElement('button');
-	btnenviar.innerHTML = 'Enviar Datos'
-	inputName.className = 'form-control';
-	divgroup.appendChild(spanName);
-	divgroup.appendChild(inputName);
-	form.appendChild(divgroup);
-	form.appendChild(btnenviar);
+	app!.innerHTML = `
+	<form >
+		<div class="input-group mb-3">
+			<span class="input-group-text">Nombre</span>
+			<input id="first_name" type="text" aria-label="First name" class="form-control">
+			<span class="input-group-text">Apellido</span>
+			<input id="last_name" type="text" aria-label="Last name" class="form-control">
+			<span class="input-group-text">N° Telefono</span>
+			<input id="phone" type="text" aria-label="phone" class="form-control">
+		</div>	
+		<div class="input-group mb-3">
+			<span class="input-group-text">Empresa</span>
+			<input id="empresa" type="text" aria-label="empresa" class="form-control">
+			<span class="input-group-text">Cuit/Cuil</span>
+			<input id="cuil" type="text" aria-label="cuit cuil" class="form-control">
+			<select id="pais" class="form-select" aria-label="Default select example">
+				<option selected>Pais</option>
+				<option value="argentina">argentina</option>
+				<option value="brazil">Brazil</option>
+				<option value="uruguay">Paraguay</option>
+			</select>	
+			</div>	
+		<div class="input-group mb-3">
+			<span class="input-group-text">Localidad</span>
+			<input id="localidad" type="text" aria-label="localidad" class="form-control">
+			<span class="input-group-text">Codigo Postal</span>
+			<input id="cp" type="text" aria-label="cp" class="form-control">
+			<span class="input-group-text">Direccion</span>
+			<input id="direccion" type="text" aria-label="direccion" class="form-control">
+		</div>	
+		<button id="btnenviar">Enviar Datos</button>
+	</form>
+	`;	
+	
+	const btnenviar = $('#btnenviar');
 
-	app!.innerHTML = '';
-	app!.appendChild(form);
+	btnenviar?.addEventListener('click',  async (e : Event) => {
+		e.preventDefault();
 
-	btnenviar?.addEventListener('click', (e : Event) => {
-		console.log("ALGO..");
-		const first_name = document.querySelector<HTMLInputElement>('#first_name');
-		console.log(first_name?.value);
-		var data = new FormData();
-		data.append('first_name', `${first_name?.value}` );
-		// data.append('pwd', 'password');
-		// data.append('organization', 'place');
-		// data.append('requiredkey', 'key');
-		console.log(data);
-		var xhr = new XMLHttpRequest();
-		xhr.open('POST', 'http://127.0.0.1:5173/?#', true);
-		xhr.onload = function () {
-			// do something to response
-			console.log(this.responseText);
-		};
-		xhr.send(data);
+		let inputs = cargarInputs();
+		let errores = validarInputs(inputs);
+		
+		if(errores.length == 0) {
+			
+			axios.get('https://api-user-production.up.railway.app/')
+			.then(function (response) {
+				// handle success
+				console.log(response['data']);
+			})
+			.catch(function (error) {
+				// handle error
+				console.log(error);
+			})
+			.finally(function () {
+				// always executed
+			});
+
+			axios.post('https://api-user-production.up.railway.app/usuarios/', inputs )
+			.then(function (response) {
+				console.log(response['data']);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+
+		}else{	
+			console.table(errores);
+		}
+
 	});
 
 	app!.classList.add('animate__animated');
@@ -88,6 +165,41 @@ crearCliente?.addEventListener('click', () => {
 	crearCliente!.classList.add('active');
 });
 
+function $(idElement : any){
+	return document.querySelector(idElement);
+}
+
+
+function cargarInputs(){
+	return {
+		nombre : $('#first_name').value.toLowerCase(),
+		apellido : $('#last_name').value.toLowerCase(),
+		numeroTelefono : $('#phone').value,
+		pais : $('#pais').value.toLowerCase(),
+		localidad : $('#localidad').value.toLowerCase(),
+		codPostal : $('#cp').value,	
+		direccion : $('#direccion').value.toLowerCase(),
+	}	
+}
+
+function validarInputs(inputs : any) {
+	const err : any = [];
+	if(inputs.nombre.length == 0 || inputs.apellido.length == 0) {
+		err.push("El primer nombre o apellido estan vacio");
+	}	
+	if(inputs.codPostal.length != 4) {
+		err.push("el cuil debe tener una longitud de 11 digitos");
+	}
+
+	return err;
+}
+
+function crearNodo(nodoDato : any){
+	let data = document.createElement('td');
+	data.setAttribute("border", "2")
+	data.innerHTML = nodoDato;
+	return data;
+}
 
 /**
  first_name
